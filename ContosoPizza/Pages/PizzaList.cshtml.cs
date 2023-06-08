2e9006aa07afd9599c70927fc65ce22e53810ffa
Mannan Shaikh <arsalaanshaikh8@gmail.com>
2023-06-08 13:15:36 +0530

    added new functionality

--- ContosoPizza/Pages/PizzaList.cshtml.cs
+++ ContosoPizza/Pages/PizzaList.cshtml.cs
@@ -7,6 +7,8 @@ namespace ContosoPizza.Pages
 {
     public class PizzaListModel : PageModel
     {
+        [BindProperty]
+        public Pizza NewPizza { get; set; } = default!;
         private readonly PizzaService _service;
         public IList<Pizza> PizzaList { get; set; } = default!;
 
@@ -18,6 +20,23 @@ namespace ContosoPizza.Pages
         public void OnGet()
         {
             PizzaList = _service.GetPizzas();
+        }
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid || NewPizza == null)
+            {
+                return Page();
+            }
+
+            _service.AddPizza(NewPizza);
+
+            return RedirectToAction("Get");
+        }
+        public IActionResult OnPostDelete(int id)
+        {
+            _service.DeletePizza(id);
+
+            return RedirectToAction("Get");
         }
     }
 }
